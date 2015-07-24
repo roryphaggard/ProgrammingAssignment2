27f248fde6e0ef38ef6f174fb9d716f6318c771d
Rory P Haggard <rory.haggard@gmail.com>
2015-07-24 15:48:28 -0400

    Final commit, ready for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Matrix calculation can be costly from a computational perspective.
 ## These functions will allow you to work from a cache when available. 
 
-## This function supply a cached inverse matrix to cacheSolve if available
+## This function can supply a cached inverse matrix to cacheSolve if available
 makeCacheMatrix <- function(x = matrix()) {
     initialMatrix <- function()
         x <<- y
@@ -9,24 +9,25 @@ makeCacheMatrix <- function(x = matrix()) {
     getMatrix <- function() x
     setCache <- function(cache) m <<- m
     getCache <- function() m
+    ## List the available functions when calling this function directly
     list(setCache = setCache, 
          getCache = getCache,
          initialMatrix = initialMatrix)
     }
 
-
 ## This function will provide the inverse of the input matrix
 cacheSolve <- function(x, ...) {
-    library(MASS)
+    ## Retrieve cache if available
     m <- x$getCache
     if(!is.null(m)){
         message("retrieving cached matrix")
         return(m)
-    }
-    matrix <- x$getMatrix
-    invertMatrix <- ginv(matrix)
+    }else{
+    ## If no cache is available, invert the matrix provided and then cache it  
+    invertMatrix <- solve(x$`initialMatrix`)
     x$setCache(invertMatrix)    
     m <- invertMatrix
     ## Return a matrix that is the inverse of 'x'
     return(m)
+    }
 }
